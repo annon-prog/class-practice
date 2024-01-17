1980d2017041b33663b8935378ca1661def1ba39
annon-prog <fidelisdela.wambui@gmail.com>
2024-01-17 23:05:51 +0300

    Task: Final submission

--- classes/person.js
+++ classes/person.js
@@ -1,4 +1,31 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  static introducePeople(people) {
+     if (!Array.isArray(people)) {
+       throw new Error("introducePeople only takes an array as an argument.");
+    };
+    
+    people.forEach(person => {
+       if (!(person instanceof Person)) {
+        throw new Error("All items in array must be Person class instances.");
+      } else {
+       person.introduce();
+      }
+    })
+   
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
